93605622e898ce18e9f110ba16e47fc410ce77f1
xnslx <xnslx89@gmail.com>
2020-10-17 14:30:57 -0500

    validation

--- routes/auth.js
+++ routes/auth.js
@@ -3,17 +3,32 @@ const { body } = require('express-validator');
 const router = express.Router();
 
 const authController = require('../controllers/auth');
+const User = require('../models/users');
 
 router.get('/signup', authController.getSignup)
 
-router.post('/signup', authController.postSignup)
+router.post('/signup', [
+    body('name')
+    .trim(),
+    body('email')
+    .isEmail()
+    .withMessage('Please enter a valid email')
+    .custom((value, { req }) => {
+        return User.findOne({ email: value })
+            .then(userDoc => {
+                if (userDoc) {
+                    return Promise.reject('E-mail already in use')
+                }
+            })
+    })
+], authController.postSignup)
 
 router.get('/login', authController.getLogin);
 
 router.post('/login', [
     body('email')
     .isEmail()
-    .withMessage('Please enter a valid email')
+    .withMessage('Please enter a valid email.')
     .normalizeEmail(),
     body('password', 'Password has to be valid.')
     .isLength({ min: 5, max: 20 })
