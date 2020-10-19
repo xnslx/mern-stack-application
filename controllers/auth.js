34b130db14d2ef1737a2bfca5e6e775f5973d938
xnslx <xnslx89@gmail.com>
2020-10-18 21:52:13 -0500

    refactor

--- controllers/auth.js
+++ controllers/auth.js
@@ -59,26 +59,29 @@ exports.postLogin = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     // console.log(errors.errors)
+    let loadedUser;
     if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() })
     }
     User.findOne({ email: email }).then(user => {
         if (!user) {
             return res.status(401).json('Email not found!')
-        } else {
-            bcrypt
-                .compare(password, user.password)
-                .then(isMatch => {
-                    if (!isMatch) {
-                        res.json('Password is wrong!')
-                    } else {
-                        const username = req.body.email;
-                        const user = { name: username }
-                        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-                        res.json({ token: token })
-                    }
-                })
         }
+        loadedUser = user;
+        bcrypt
+            .compare(password, user.password)
+            .then(isMatch => {
+                if (!isMatch) {
+                    res.json('Password is wrong!')
+                } else {
+                    const user = {
+                        email: loadedUser.email,
+                        userId: loadedUser._id.toString()
+                    }
+                    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+                    res.json({ token: token, userId: user.userId })
+                }
+            })
     })
 }
 
