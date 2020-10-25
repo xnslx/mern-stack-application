c7b9d24602b23de81202c75db5d96bd5641d9d42
xnslx <xnslx89@gmail.com>
2020-10-24 23:48:42 -0500

    refactor

--- routes/auth.js
+++ routes/auth.js
@@ -15,6 +15,7 @@ router.post('/signup', [
     .isEmail()
     .withMessage('Please enter a valid email')
     .custom((value, { req }) => {
+        console.log('value', value)
         return User.findOne({ email: value })
             .then(userDoc => {
                 if (userDoc) {
@@ -26,6 +27,15 @@ router.post('/signup', [
     body('password', 'Password has to be valid.')
     .isLength({ min: 5, max: 20 })
     .isAlphanumeric()
+    .trim(),
+    check('confirmPassword')
+    .custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Password have to be matched!')
+        } else {
+            return true;
+        }
+    })
     .trim()
 ], authController.postSignup)
 
