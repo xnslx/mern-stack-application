798d2efe38671b6a283b651369897974c75d0d11
xnslx <xnslx89@gmail.com>
2020-11-13 20:53:23 -0600

    refactor

--- client/src/component/resetPassword/ResetPassword.js
+++ client/src/component/resetPassword/ResetPassword.js
@@ -1,22 +1,31 @@
 import React, {useState, useEffect} from 'react';
 import {withRouter} from 'react-router';
+import {connect} from 'react-redux';
 import axios from 'axios';
 
 const ResetPassword = (props) => {
-    console.log('props', props)
+    // console.log('props', props)
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
-    // const token = props.match.params.token;
-    // useEffect(() => {
-    //     axios.get(`/updatepassword/${token}`).then(result => {
-    //         console.log('result',result)
-    //     }).catch(err => {
-    //         console.log(err)
-    //     })
-    // },[])
+    const [error, setError] = useState({})
+    const verifiedPassword = {
+        password:password,
+        confirmPassword: confirmPassword
+    }
+    const token = props.match.params.token;
+
+    const resetPasswordHandler = (e) => {
+        e.preventDefault();
+        axios.post(`/updatepassword/${token}`, verifiedPassword).then(result => {
+            console.log('result',result)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
     return (
         <div>
-            <form action="">
+            <form action="" onSubmit={resetPasswordHandler}>
                 <label htmlFor="password">Password</label>
                 <input 
                     type="password" 
