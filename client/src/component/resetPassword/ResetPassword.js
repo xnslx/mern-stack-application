6eb18aefcba3994f914e4d8d189122fcccfcc6bc
xnslx <xnslx89@gmail.com>
2020-11-17 22:51:39 -0600

    redux to reset password

--- client/src/component/resetPassword/ResetPassword.js
+++ client/src/component/resetPassword/ResetPassword.js
@@ -1,47 +1,46 @@
 import React, {useState, useEffect} from 'react';
 import {withRouter} from 'react-router';
-// import {connect} from 'react-redux';
-// import {resetPassword} from '../../action/action';
+import {connect} from 'react-redux';
+import {resetPassword} from '../../action/action';
 import axios from 'axios';
 
 const ResetPassword = (props) => {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState({});
-    // const [resetTokenExpiration, setResetTokenExpiration] = useState('');
-    const [userId, setUserId] = useState('')
-    const [formData, setFormData] = useState({
-        password:'', confirmPassword:''
-    })
-    
-    const verifiedPassword = {
-        password:password,
-        confirmPassword: confirmPassword
-    }
     const token = props.match.params.token;
 
-    const changeHandler = (e) => {
-        setFormData({...formData, [e.target.value]: e.target.value})
+    useEffect(() => {
+        getErrorMessage()
+    }, [props.error])
+
+    const getErrorMessage = () => {
+        if(props.error !== null) {
+        setError(props.error.errors);
+        }
     }
+
+    console.log('error', error)
     
     const resetPasswordHandler = (e) => {
-        console.log('i am clicked')
         e.preventDefault();
-        axios.post('/updatepassword', {
-            password:password,confirmPassword:confirmPassword,passwordToken:token
-        }).then(result => {
-            console.log('result', result)
-            if(result.data.message === 'password updated') {
-                props.history.push('/login')
-            }
-        }).catch(err => {
-            console.log(err)
-        })
+        props.dispatch(resetPassword({password: password, confirmPassword: confirmPassword, passwordToken: token},props.history))
+        // axios.post('/updatepassword', {
+        //     password:password,confirmPassword:confirmPassword,passwordToken:token
+        // }).then(result => {
+        //     console.log('result', result)
+        //     if(result.data.message === 'password updated') {
+        //         props.history.push('/login')
+        //     }
+        // }).catch(err => {
+        //     console.log(err)
+        // })
     }
 
 
     return (
         <div>
+        {error.length > 0? <ul>{error.map((err,index) => <li key={index}>{err.msg}</li>)}</ul> : null}
             <form action="" onSubmit={resetPasswordHandler}>
                 <div>
                     <label htmlFor="password">Password</label>
@@ -67,13 +66,13 @@ const ResetPassword = (props) => {
     )
 };
 
-// const mapStateToProps = (state) => {
-//     console.log('state', state)
-//     return {
-//         auth: state.auth,
-//         error: state.error,
-//         backendData: state.backendData.backendData
-//     }
-// }
+const mapStateToProps = (state) => {
+    console.log('state', state)
+    return {
+        auth: state.auth,
+        error: state.error.message,
+        backendData: state.backendData.backendData
+    }
+}
 
-export default withRouter(ResetPassword);
+export default connect(mapStateToProps)(withRouter(ResetPassword));
