661e2a7f143f78feecbcb2372a41de0054e1997f
xnslx <xnslx89@gmail.com>
2020-11-23 21:52:33 -0600

    refactor

--- client/src/component/products/Products.js
+++ client/src/component/products/Products.js
@@ -1,7 +1,6 @@
 import React, {useState,useEffect} from 'react';
 import axios from 'axios';
 import ProductDetail from '../products/ProductDetail';
-// import {Switch, Route} from 'react-router-dom';
 import {Link} from 'react-router-dom';
 
 const Products = () => {
@@ -17,14 +16,6 @@ const Products = () => {
             })
     }, [])
 
-    // const productDetailHandler = (prodId) => {
-    //     console.log('prodId', prodId)
-    //     axios.get('/products/productslist/'+ prodId).then(product => {
-    //         console.log(product)
-    //     }).catch(err => {
-    //         console.log(err)
-    //     })
-    // }
     return (
         <>
             <div>
