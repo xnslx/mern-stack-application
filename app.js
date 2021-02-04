f7759f771f1e74f5bf647f224099bbd115b26b8c
xnslx <xnslx89@gmail.com>
2021-02-03 22:29:32 -0600

    deployment

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ mongoose.connect(dbUrl, {
 })
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path_join(__dirname, '../client/build')));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
