090ac42d79d25cf2de51573420e543b10bf1d37c
xnslx <xnslx89@gmail.com>
2021-02-05 00:25:10 -0600

    deploy

--- app.js
+++ app.js
@@ -32,12 +32,6 @@ app.use('/products', productsRoutes);
 
 
 
-app.use((req, res, next) => {
-    const error = new Error('Not found!');
-    error.status = 404;
-    next(error);
-})
-
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
@@ -65,3 +59,10 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.listen(port)
+
+
+app.use((req, res, next) => {
+    const error = new Error('Not found!');
+    error.status = 404;
+    next(error);
+})
